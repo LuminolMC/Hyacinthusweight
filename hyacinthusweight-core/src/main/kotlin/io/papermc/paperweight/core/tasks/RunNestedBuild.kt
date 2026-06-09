504bb76669717f52e6ef4615ab196267c5022a54
Helvetica Volubi <suisuroru@blue-millennium.fun>
2026-06-10 02:40:39 +0800

    fallback changes

--- hyacinthusweight-core/src/main/kotlin/io/papermc/paperweight/core/tasks/RunNestedBuild.kt
+++ hyacinthusweight-core/src/main/kotlin/io/papermc/paperweight/core/tasks/RunNestedBuild.kt
@@ -22,12 +22,13 @@
 
 package io.papermc.paperweight.core.tasks
 
-import io.papermc.paperweight.tasks.*
-import io.papermc.paperweight.util.*
-import io.papermc.paperweight.util.constants.*
-import java.io.File
-import kotlin.io.path.*
-import org.gradle.StartParameter
+import io.papermc.paperweight.tasks.BaseTask
+import io.papermc.paperweight.util.constants.PAPERWEIGHT_DEBUG
+import io.papermc.paperweight.util.constants.UPSTREAM_WORK_DIR_PROPERTY
+import io.papermc.paperweight.util.path
+import io.papermc.paperweight.util.upstreamsDirectory
+import kotlin.collections.set
+import kotlin.io.path.absolutePathString
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.internal.StartParameterInternal
 import org.gradle.api.provider.SetProperty
@@ -37,7 +38,6 @@ import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.UntrackedTask
 import org.gradle.internal.build.NestedRootBuildRunner
-import org.gradle.internal.classpath.ClassPath
 import org.gradle.internal.service.ServiceRegistry
 
 @UntrackedTask(because = "Nested build does it's own up-to-date checking")
@@ -68,72 +68,11 @@ abstract class RunNestedBuild : BaseTask() {
 
         params.systemPropertiesArgs[PAPERWEIGHT_DEBUG] = System.getProperty(PAPERWEIGHT_DEBUG, "false")
 
-        try {
-            runTask(params)
-        } catch (_: Exception) {
-            try {
-                params.projectDir?.let { modifyDownstreamSettings(it) }
-            } catch (modifyException: Exception) {
-                logger.warn("Failed to modify downstream build.gradle.kts: ${modifyException.message}")
-            }
-            runTask(params)
-        }
-    }
-
-    fun runTask(params: StartParameter) {
         NestedRootBuildRunner::class.java.getDeclaredMethod(
             "runNestedRootBuild",
             String::class.java,
             StartParameterInternal::class.java,
-            ServiceRegistry::class.java,
-            ClassPath::class.java
-        ).invoke(null, null, params, services, null)
-    }
-
-    private fun modifyDownstreamSettings(projectDir: File) {
-        val buildFile = File(projectDir, "build.gradle.kts")
-        if (!buildFile.exists()) {
-            return
-        }
-
-        var content = buildFile.readText()
-
-        val currentVersion = project.version.toString()
-
-        val oldPatcherPluginId = """id\(["']io\.papermc\.paperweight\.patcher["']\)\s+version\s+["'][^"']+["']"""
-        val newPatcherPluginId = """id("moe.luminolmc.hyacinthusweight.patcher") version "$currentVersion""""
-
-        val oldCorePluginId = """id\(["']io\.papermc\.paperweight\.core["']\)\s+version\s+["'][^"']+["']"""
-        val newCorePluginId = """id("moe.luminolmc.hyacinthusweight.core") version "$currentVersion""""
-
-        var modified = false
-
-        if (content.contains(Regex(oldPatcherPluginId))) {
-            content = content.replace(Regex(oldPatcherPluginId), newPatcherPluginId)
-            modified = true
-        }
-
-        if (content.contains(Regex(oldCorePluginId))) {
-            content = content.replace(Regex(oldCorePluginId), newCorePluginId)
-            modified = true
-        }
-
-        val weightVersionPattern = """version\s+weightVersion"""
-        if (content.contains(Regex(weightVersionPattern))) {
-            content = content.replace(Regex(weightVersionPattern), """version "$currentVersion"""")
-            modified = true
-        }
-
-        if (modified) {
-            val repositoriesMatch = Regex("""repositories\s*\{""").find(content)
-            if (repositoriesMatch != null && !content.contains("repo.menthamc.org")) {
-                content = content.substring(0, repositoriesMatch.range.last + 1) +
-                    "\n        maven(\"https://repo.menthamc.org/repository/maven-public/\")" +
-                    content.substring(repositoriesMatch.range.last + 1)
-            }
-
-            buildFile.writeText(content)
-            logger.lifecycle("Modified downstream build.gradle.kts to use hyacinthusweight plugins version $currentVersion")
-        }
+            ServiceRegistry::class.java
+        ).invoke(null, null, params, services)
     }
 }
